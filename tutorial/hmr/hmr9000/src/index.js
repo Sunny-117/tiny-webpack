7b7118a4fb1a033e6410f1a85bee39d39afde65c
Sunny-117 <zhiqiangfu6@gmail.com>
2024-12-13 11:44:53 +0800

    remove files

--- tutorial/hmr/hmr9000/src/index.js
+++
@@ -1,9 +0,0 @@
-let render = ()=>{
-    let title = require('./title.js');
-    document.getElementById('root').innerText = title;
-}
-render();
-if(module.hot){
-    //可以接收并处理哪些模块的变更
-    module.hot.accept(["./title.js"],render);
-}
--- tutorial/hmr/hmr9000/webpack/hot/dev-server.js
+++
@@ -1,6 +0,0 @@
-var hotEmitter = require('../../webpack/hot/emitter');
-hotEmitter.on('webpackHotUpdate',(currentHash)=>{
-  console.log('dev-server收到了最新的hash值',currentHash);
-  //进行真正的热更新检查
-  //hotCheck();
-});
